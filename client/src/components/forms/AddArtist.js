8a961d7b8075bc4a8372fd96ea1d38fdff6a6f94
laptop <jaspreetkharoud28@gmail.com>
2020-04-16 18:03:23 -0700

    Initial commit

---
+++ client/src/components/forms/AddArtist.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/react-hooks'
+
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import { v4 as uuidv4 } from 'uuid'
+
+import { ADD_ARTIST, GET_ARTISTS } from '../../queries/index'
+
+const AddArtist = () => {
+
+  const [id] = useState(uuidv4())
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  const [addArtist] = useMutation(ADD_ARTIST, {
+    update(cache, { data: { addArtist } }) {
+      const { artists } = cache.readQuery({ query: GET_ARTISTS })
+      cache.writeQuery({
+        query: GET_ARTISTS,
+        data: { artists: artists.concat([addArtist]) }
+      })
+    }
+  })
+
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault()
+        addArtist({
+          variables: {
+            id,
+            firstName,
+            lastName
+          },
+          optimisticResponse: {
+            __typename: 'Mutuation',
+            addContact: {
+              __typename: 'Artist',
+              id,
+              firstName,
+              lastName
+            }
+          },
+          update: (proxy, { data: { addArtist } }) => {
+            const data = proxy.readQuery({ query: GET_ARTISTS })
+            proxy.writeQuery({
+              query: GET_ARTISTS,
+              data: {
+                ...data,
+                artists: [...data.artist, addArtist]
+              }
+            })
+          }
+        })
+      }}
+    >
+   
+      <TextField
+        label='First Name'
+        placeholder='i.e. John'
+        margin='normal'
+        onChange={e => setFirstName(e.target.value)}
+        variant='outlined'
+        style={{ margin: '10px' }}
+      />
+      <TextField
+        label='Last Name'
+        placeholder='i.e. Smith'
+        onChange={e => setLastName(e.target.value)}
+        margin='normal'
+        variant='outlined'
+        style={{ margin: '10px' }}
+      />
+      <Button
+        type='submit'
+        variant='contained'
+        color='primary'
+        style={{ marginTop: '20px', alignItems: 'center', marginLeft: '10px' }}
+      >
+        Add Artist
+      </Button>
+    </form>
+  )
+}
+
+export default AddArtist
---
+++ client/src/components/listItems/Artist.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react'
+
+import Button from '@material-ui/core/Button'
+import CardActions from '@material-ui/core/CardActions'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import { makeStyles } from '@material-ui/core/styles'
+import  { useState } from 'react'
+import RemoveArtist from '../buttons/RemoveArtist'
+import DisplayCard from '../cards/DisplayCard'
+import UpdateArtist from '../forms/UpdateArtist'
+
+const useStyles = makeStyles({
+  label: {
+    textDecoration: 'none'
+  }
+})
+
+const Artist = props => {
+  const classes = useStyles()
+  const [id] = useState(props.id)
+  const [firstName, setFirstName] = useState(props.firstName)
+  const [lastName, setLastName] = useState(props.lastName)
+  const [editMode, setEditMode] = useState(false)
+
+  const handleButtonClick = () => {
+    setEditMode(!editMode)
+  }
+
+  const fullName = () => {
+    return `${firstName} ${lastName}`
+  }
+
+  const updateStateVariable = (variable, value) => {
+    switch (variable) {
+      case 'firstName':
+        setFirstName(value)
+        break
+      case 'lastName':
+        setLastName(value)
+        break
+      default:
+        break
+    }
+  }
+
+  return (
+    <DisplayCard >
+      <Fragment>
+      {editMode ? (
+        <UpdateArtist
+          id={props.id}
+          firstName={props.firstName}
+          lastName={props.lastName}
+          onButtonClick={handleButtonClick}
+          updateStateVariable={updateStateVariable}
+        />
+      ) : (
+        <ListItem>
+          <ListItemText primary={fullName()} />
+          <Button
+            onClick={() => setEditMode(true)}
+            variant='contained'
+            style={{ margin: '5px' }}
+          >
+            Edit
+          </Button>
+          <RemoveArtist
+            id={props.id}
+            firstName={props.firstName}
+            lastName={props.lastName}
+          />
+        </ListItem>
+      )}
+        <CardActions>
+          <Button color='primary' size='small' variant='outlined'>
+            Learn More
+          </Button>
+        </CardActions>
+      </Fragment>
+    </DisplayCard>
+  )
+}
+
+export default Artist
